59d44935c6b31c681fc16c76d5c7bc432818c2a3
Yan Carlos <yan.c.oliveira08@gmail.com>
2025-05-17 16:17:59 -0300

    Exemplo Biblioteca e print
    Exemplo de como usar a biblioteca com o print, alem de mostrar como deve ser a estrutura de um programa java

---
+++ Java_Estudos/introducao/ExemploUm.java
@@ -0,0 +1,11 @@
+package introducao;
+import java.util.Date;
+
+public class ExemploUm{
+
+    public static void main(String[] args){
+        Date relogio = new Date();
+        System.out.print("A hora bonita é: ");
+        System.out.print(relogio.toString());
+    }
+}
